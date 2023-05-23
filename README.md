6ca34927b3bde912e43103403e4162df8f0de102
Guinevere Saenger <guinevere@pulumi.com>
2023-05-23 13:41:24 -0700

    Create minimal repro files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # 573-repro
 Example of using pulumi-docker with GitHub Actions
+
+This repo is a minimal typescript program that runs `pulumi up` in a GitHub Action workflow.
+
+It creates an AWS ECR repository, a docker Image using `FROM cocalc/cocalc` to ensure we get a large image that takes 
+time to build, and builds and deploys this image to the created repository using `pulumi/actions@v3`.
+
+## Setup
+
+You must have an AWS account and a Pulumi organization.
+
+Ensure the following Action secrets are set:
+
+AWS keys:
+```
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_CI_ROLE_ARN
+```
+
+Pulumi access token:
+```
+TEST_PULUMI_ACCESS_TOKEN
+```
+
+This repo uses `us-west-2` for `AWS_REGION`; you can change the action environment to suit your needs.
+
+## Use
+
+Fork this repo and run the `test-docker-pulumi-up.yml` Workflow via dispatch against `main`.
---
+++ docker-test/index.ts
@@ -0,0 +1,32 @@
+import * as aws from "@pulumi/aws";
+import * as docker from "@pulumi/docker";
+
+// Create a private ECR registry.
+const repo = new aws.ecr.Repository("my-repo",{
+    forceDelete: true,
+});
+
+// Get registry info (creds and endpoint) so we can build/publish to it.
+const registryInfo = repo.registryId.apply(async (id: any) => {
+    const credentials = await aws.ecr.getCredentials({ registryId: id });
+    const decodedCredentials = Buffer.from(credentials.authorizationToken, "base64").toString();
+    const [username, password] = decodedCredentials.split(":");
+    if (!password || !username) {
+        throw new Error("Invalid credentials");
+    }
+    return {
+        server: credentials.proxyEndpoint,
+        username: username,
+        password: password,
+    };
+});
+
+// Build and publish the image.
+const image = new docker.Image("my-image", {
+    imageName: repo.repositoryUrl,
+    registry: registryInfo,
+});
+
+// Export the resulting image name
+export const imageName = image.imageName;
+export const repoDigest = image.repoDigest;
